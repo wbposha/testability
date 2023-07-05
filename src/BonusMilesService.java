49695a2219b6cd71b8e862bbd3b6c2a767de586d
wbposha <135736179+wbposha@users.noreply.github.com>
2023-07-06 00:22:17 +0400

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class BonusMilesService {
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
+    }
+}
+
+
+
+### Class
+
+public class BonusMilesService {
+
+    public int calculate(int price) {
+       int result;
+        int i = price = 10_000;
+        {
+           result = price / 20;
+
+       }
+       return result;
+    }
+
+
 }
