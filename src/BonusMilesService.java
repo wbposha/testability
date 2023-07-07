36c4cd7707db60fb1615fa5082cd8d5a85f636e2
wbposha <135736179+wbposha@users.noreply.github.com>
2023-07-07 23:51:12 +0400

    Add files via upload

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,11 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
-        int miles = service.calculate(price); // должно получиться 500
-        System.out.println(miles);
+public class BonusMilesService {
+
+    public int calculate(int price) {
+       int result;
+        int i = price = 10_000;
+        {
+           result = price / 20;
+
+       }
+       return result;
     }
+
+
 }
-
-
-
