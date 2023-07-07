4b8a5ddfbc91ebd9c6010ef424e1b551205192f3
wbposha <135736179+wbposha@users.noreply.github.com>
2023-07-07 23:50:17 +0400

    Update BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -9,19 +9,3 @@ public class Main {
 
 
 
-### Class
-
-public class BonusMilesService {
-
-    public int calculate(int price) {
-        int result;
-        int i = price = 10_000;
-        {
-           result = price / 20;
-
-       }
-       return result;
-    }
-
-
-}
